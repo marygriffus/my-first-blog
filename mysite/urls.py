9cbc7a9469dfcbe49658737de4747c1347c852c3
Mary Griffus <mary.e.shultz@gmail.com>
2018-09-22 12:59:08 -0400

    Initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,9 @@
+"""mysite URL Configuration
+"""
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
